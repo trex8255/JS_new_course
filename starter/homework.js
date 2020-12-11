972c1b881ee9a8229cb6965a58d3884a2f118a8b
taewoo <taewoo.lee88@gmail.com>
2020-12-12 01:37:39 +0900

    going

--- starter/homework.js
+++ starter/homework.js
@@ -2,30 +2,41 @@ let country = "Korea";
 let continent = "puninsula";
 let population = 60000000;
 
-console.log(country, continent, population);
+// console.log(country, continent, population);
 
-let isIsland = true;
-let language = "Korean";
-const gender = "man"
-console.log(typeof isIsland, typeof population, typeof country, typeof language);
+// let isIsland = true;
+// let language = "Korean";
+// const gender = "man"
+// console.log(typeof isIsland, typeof population, typeof country, typeof language);
 
-//Basic operators
-console.log(population/2);
-console.log(population+1);
-const Finland = 6000000;
-console.log(population>Finland);
-const average_population = 33000000;
-console.log(average_population<population);
-const description = "Portugal is in Europe, and its 11 million people speak portuguese"
-//Coding Challenge #1
-const MarkMess = 78;
-const MakrkHeight = 1.69;
-const JohnMess = 92;
-const JohnHeight = 1.95
+// //Basic operators
+// console.log(population/2);
+// console.log(population+1);
+// const Finland = 6000000;
+// console.log(population>Finland);
+// const average_population = 33000000;
+// console.log(average_population<population);
+// const description = "Portugal is in Europe, and its 11 million people speak portuguese"
+// //Coding Challenge #1
+// const MarkMess = 78;
+// const MakrkHeight = 1.69;
+// const JohnMess = 92;
+// const JohnHeight = 1.95
 
-const MarkBMI = MarkMess/MakrkHeight**2;
-console.log(MarkBMI);
-const JohnBMI = JohnMess/JohnHeight**2;
-console.log(JohnBMI);
-const MarkHigherBMI = MarkBMI>JohnBMI;
-console.log(MarkHigherBMI);
+// const MarkBMI = MarkMess/MakrkHeight**2;
+// console.log(MarkBMI);
+// const JohnBMI = JohnMess/JohnHeight**2;
+// console.log(JohnBMI);
+// const MarkHigherBMI = MarkBMI>JohnBMI;
+// console.log(MarkHigherBMI);
+
+// //Strings and template literals
+// console.log(`${country} is in ${continent}, and its ${population} people speak ${language}`)
+
+//If statement
+
+if (population>33000000) {
+     console.log(`${country}'s population is above average`)
+}else {
+     console.log(`${country}'s population is ${33000000-population} below average`)
+};
